d328dcfbca126b6997ebcceb72355b78bba30ae7
TheCodeWizard27 <benny.schaerer@gmail.com>
2024-12-31 14:06:17 +0100

    Ignore this.

--- src/scenes/main/main.gd
+++ src/scenes/main/main.gd
@@ -1,12 +1,16 @@
 extends Node3D
 
 @export var body: RigidBody3D
+@export var height_map: Texture2D
 
-# Called when the node enters the scene tree for the first time.
+@onready var map_collider: CollisionShape3D = $"StaticBody3D/CollisionShape3D"
+
 func _ready() -> void:
-	pass # Replace with function body.
+	var image = height_map.get_image()
+	image.decompress()
+	image.convert(Image.FORMAT_RF)
+	(map_collider.shape as HeightMapShape3D).update_map_data_from_image(image, 0.0, 10.0)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * 1000 * delta
+	body.apply_force(Vector3(input_direction.x, 0, input_direction.y))
