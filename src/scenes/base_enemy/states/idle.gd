f5830ba7b0ee9bade55a6cd4bf8c38a1aefa597f
neyrision <neyrision@gmail.com>
2025-01-08 19:30:08 +0100

    commit from tuesday at school

--- src/scenes/base_enemy/states/idle.gd
+++ src/scenes/base_enemy/states/idle.gd
@@ -7,5 +7,6 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func physics_update(delta: float) -> void:
+	baseEnemy.chase();
 	pass
