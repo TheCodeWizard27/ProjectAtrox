39d4e8401a28d3f18d1645928232c3187f98a2bb
neyrision <neyrision@gmail.com>
2025-02-11 12:23:57 +0100

    implemented more of the baseenemy

--- src/scenes/base_enemy/base_enemy.gd
+++ src/scenes/base_enemy/base_enemy.gd
@@ -11,8 +11,12 @@ var enemySpeed: float = 3.0;
 func _ready() -> void:
 	pass # Replace with function body.
 
-func chase() -> void: 
-	_body.velocity = (player._body.position - _body.position) * 3
+func chase(delta: float) -> void: 
+	var direction = _body.global_position.direction_to(player._body.global_position)
+	_body.velocity = direction * enemySpeed
+	_body.velocity.y = -Globals.GRAVITY
+	
+
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,3 +25,10 @@ func _process(delta: float) -> void:
 	
 func _physics_process(delta: float) -> void:
 	_state.physics_process(delta)
+	_body.move_and_slide()
+	#handle_collision()
+
+#func handle_collision() -> void:
+	#for i in _body.get_slide_collision_count():	
+		##var collision = _body.get_slide_collision(i)
+		##print("I collided with ", collision.get_collider().name)
