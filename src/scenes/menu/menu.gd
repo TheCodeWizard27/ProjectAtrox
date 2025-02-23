4f3b5f17114218c79b70cbfb17b099893233c548
Yillie <tobia.schaerli@hotmail.com>
2025-02-23 18:33:50 +0100

    main menu

---
+++ src/scenes/menu/menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/scenes/main/main.tscn")
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/scenes/menu/options/options_menu.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
